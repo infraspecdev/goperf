987e9eacbde6766214f9575407104ab014b1fa7b
Akshay Francis <akshay.int26@infraspec.dev>
2026-02-18 16:35:54 +0530

    fix(main): exit with non-zero code on command failure

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/infraspecdev/goperf/cmd"
+import (
+	"os"
+
+	"github.com/infraspecdev/goperf/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
